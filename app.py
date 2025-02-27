d3cdc9bbdfff7d11892ca906d4a693a6260f772e
Matias7625 <ortizzapatamatias@gmail.com>
2025-02-27 10:22:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ st.write("Si que estamos chida!!!")
 image = Image.open("armydark5.jpg")
 
 st.image(image, caption="Interfaces multimodales")
+
+
+texto = st.text_input("Escribe algo", "Este es mi texto")
+st. write("El texto escrito es", texto)
