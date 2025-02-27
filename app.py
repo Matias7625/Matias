111101535fc5fad78ced177e3fa7069227c7f36f
Matias7625 <ortizzapatamatias@gmail.com>
2025-02-27 10:11:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.title(" Mi primera cosa")
 
 st.header("Una caraca re chida.")
 st.write("Si que estamos chida!!!")
-image = Image.open("armydarl5.jpg")
+image = Image.open("armydark5.jpg")
 
 st.image(image, caption="Interfaces multimodales")
