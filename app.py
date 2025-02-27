48c8e63898bbedddd087aa27f691bdb1df07bb62
Matias7625 <ortizzapatamatias@gmail.com>
2025-02-27 10:29:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.image(image, caption="Interfaces multimodales")
 texto = st.text_input("Escribe algo", "Este es mi texto")
 st. write("El texto escrito es", texto)
 
-col1, col2 = st.dolumns(2)
+col1, col2 = st.columns(2)
 
 with col1:
   st.subheader("Esta es una columna")
