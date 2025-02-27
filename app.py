97802db2ebbe4fb21f42b107150817fad5f37ae2
Matias7625 <ortizzapatamatias@gmail.com>
2025-02-27 10:10:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
-
+from PIL import Image
 
 st.title(" Mi primera cosa")
+
+st.header("Una caraca re chida.")
+st.write("Si que estamos chida!!!")
+image = Image.open("armydarl5.jpg")
+
+st.image(image, caption="Interfaces multimodales")
